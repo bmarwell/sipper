b51375af9126a1bb4717a472f61eaba3e3865896
Benjamin Marwell <bmarwell@apache.org>
2024-01-03 22:16:11 +0100

    [CHORE] fix license header

--- impl/src/main/java/io/github/bmarwell/sipper/impl/proto/AbstractNotifyingMessageHandler.java
+++ impl/src/main/java/io/github/bmarwell/sipper/impl/proto/AbstractNotifyingMessageHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) ${project.inceptionYear} The SIPper project team.
+ * Copyright (C) 2023-2024 The SIPper project team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
